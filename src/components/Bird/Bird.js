99f6b64ab095cca36f776b924bccd7feb07aac78
Promy <promysanjida@gmail.com>
2021-04-01 01:39:52 -0700

    database connected

--- src/components/Bird/Bird.js
+++ src/components/Bird/Bird.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Bird = ({ bird }) => {
     return (
-        <div className="col-md-3">
-            <img style={{ height: '300px' }} src={bird.imageURL} alt="" />
-            <h3>Name: {bird.name}</h3>
-            <h4>Price: {bird.price}</h4>
+        <div style ={{marginLeft: '50px', paddingLeft: '200px', paddingBottom: '30px'}} className="col-md-3">
+            <img style={{ height: '300px', width: '350px'}} src={bird.imageURL} alt="" />
+            <h4>Name: {bird.name}</h4>
+            <h5>Price: {bird.price}</h5>
+            <button className= 'btn btn-primary'>Buy Now</button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZVKrvSEid3xQutwQjGvlstqR2-RLqeqc",
+    authDomain: "bird-box-f16a6.firebaseapp.com",
+    projectId: "bird-box-f16a6",
+    storageBucket: "bird-box-f16a6.appspot.com",
+    messagingSenderId: "555231749093",
+    appId: "1:555231749093:web:81362adc0eedca74e9850c"
+  };
+  export default firebaseConfig;
